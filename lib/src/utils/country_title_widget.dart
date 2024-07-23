3a6138a56f68b48dae170f7ed2af40b26100a5b0
Samandar Rajabboyev <89295109+Samandar-Rajabboyev@users.noreply.github.com>
2024-07-23 17:41:03 +0500

    change_flag_size

--- lib/src/utils/country_title_widget.dart
+++ lib/src/utils/country_title_widget.dart
@@ -34,8 +34,8 @@ class CountryTitle extends StatelessWidget {
                   borderRadius: BorderRadius.circular(3),
                   child: Image.asset(
                     'assets/flags/${state.selectedCountryCode.countryCode.toLowerCase()}.png',
-                    height: 18,
-                    width: 34,
+                    height: 16,
+                    width: 24,
                     fit: BoxFit.cover,
                     errorBuilder: (context, error, stackTrace) =>
                         WrongFlagContainer(),
