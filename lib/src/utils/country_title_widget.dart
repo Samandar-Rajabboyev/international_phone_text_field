03d063ac13030e024de0c0f9e8aa28f5ca0953e4
s.rajabboyev <s.rajabboyev@uic.group>
2024-07-24 17:22:38 +0500

    fix: add flutter_svg package.

--- lib/src/utils/country_title_widget.dart
+++ lib/src/utils/country_title_widget.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:international_phone_text_field/src/controller/phone_controller_bloc.dart';
-import 'package:international_phone_text_field/src/utils/wrong_flag_container.dart';
 
 class CountryTitle extends StatelessWidget {
   CountryTitle({
     super.key,
     required this.state,
-    this.style = const TextStyle(
-        fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+    this.style = const TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
     required this.onTap,
     required this.notFoundCountryMessage,
     this.countryTextStyle,
@@ -27,26 +26,41 @@ class CountryTitle extends StatelessWidget {
       onTap: onTap,
       behavior: HitTestBehavior.opaque,
       child: AnimatedCrossFade(
-          firstChild: Row(
-            children: [
-              if (state.selectedCountryCode.countryCode.isNotEmpty)
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(3),
-                  child: Image.asset(
-                    'assets/flags/${state.selectedCountryCode.countryCode.toLowerCase()}.png',
-                    height: 20,
-                    width: 28,
-                    fit: BoxFit.fill,
-                    errorBuilder: (context, error, stackTrace) =>
-                        SvgPicture.asset('assets/ic_globe.svg'),
-                  ),
-                )
-              else
-                SvgPicture.asset('assets/ic_globe.svg'),
-              if (!inOneLine) ...[
-                SizedBox(width: 8),
-                Text(
-                  state.selectedCountryCode.country,
+        firstChild: Row(
+          children: [
+            if (state.selectedCountryCode.countryCode.isNotEmpty)
+              ClipRRect(
+                borderRadius: BorderRadius.circular(3),
+                child: Image.asset(
+                  'assets/flags/${state.selectedCountryCode.countryCode.toLowerCase()}.png',
+                  height: 20,
+                  width: 28,
+                  fit: BoxFit.fill,
+                  errorBuilder: (context, error, stackTrace) => SvgPicture.asset('assets/ic_globe.svg'),
+                ),
+              )
+            else
+              SvgPicture.asset('assets/ic_globe.svg'),
+            if (!inOneLine) ...[
+              SizedBox(width: 8),
+              Text(
+                state.selectedCountryCode.country,
+                style: countryTextStyle ??
+                    TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue,
+                    ),
+              ),
+            ]
+          ],
+        ),
+        secondChild: inOneLine
+            ? SvgPicture.asset('assets/ic_globe.svg')
+            : Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  notFoundCountryMessage,
                   style: countryTextStyle ??
                       TextStyle(
                         fontSize: 16,
@@ -54,29 +68,10 @@ class CountryTitle extends StatelessWidget {
                         color: Colors.blue,
                       ),
                 ),
-              ]
-            ],
-          ),
-          secondChild: inOneLine
-              ? SvgPicture.asset('assets/ic_globe.svg')
-              : Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    notFoundCountryMessage,
-                    style: countryTextStyle ??
-                        TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.blue,
-                        ),
-                  ),
-                ),
-          crossFadeState: state.selectedCountryCode.isNotEmpty()
-              ? CrossFadeState.showFirst
-              : CrossFadeState.showSecond,
-          duration: Duration(milliseconds: 400),
-        ),
-      
+              ),
+        crossFadeState: state.selectedCountryCode.isNotEmpty() ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        duration: Duration(milliseconds: 400),
+      ),
     );
   }
 }
